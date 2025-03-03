937b9a757b9276c923d4bddec4fa219cefa09575
aaron <agutcan1709@fp.iesromerovargas.com>
2025-03-03 20:29:14 +0100

    adawdij

--- app/main.py
+++ app/main.py
@@ -22,7 +22,7 @@ def get_db():
 def create_user(user: schemas.UserCreate, db: Session = Depends(get_db)):
     db_user = crud.get_user_by_email(db, email=user.email)
     if db_user:
-        raise HTTPException(status_code=400, detail="Email already registereddd")
+        raise HTTPException(status_code=400, detail="Email already registeredadd")
     return crud.create_user(db=db, user=user)
 
 @app.get("/users/{user_id}", response_model=schemas.User)
